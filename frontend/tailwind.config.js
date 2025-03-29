b367ba6cd441282d41a4c9dc4c1d738a188acd74
bavanagaranR <bavanab42@gmail.com>
2025-03-29 22:24:46 +0530

    after long time

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,27 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
 
-	daisyui: {
-		themes: [
-			"light",
-			{
-				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
-				},
-			},
-		],
-	},
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+          accent: "#1a1a1a",
+          neutral: "#111111",
+          "base-100": "#000000",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
 };
