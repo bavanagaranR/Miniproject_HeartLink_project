048a2cd687e79d62324353b59f512883dfc07ca3
bavanagaranR <163574985+bavanagaranR@users.noreply.github.com>
2025-04-01 17:17:06 +0530

    Update vite.config.js 2nd time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailWindcss from '@tailwindcss/vite'
+import tailWindcss from '@tailwindcss/postcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailWindcss(),react()],
