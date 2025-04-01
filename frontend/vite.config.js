38c09e41974efa5aa0cbc1979c7ffa161606b173
bavanagaranR <163574985+bavanagaranR@users.noreply.github.com>
2025-04-01 17:15:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailWindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailWindcss(),react()],
   server: {
     port: parseInt(process.env.PORT) || 3000,
     proxy: {
